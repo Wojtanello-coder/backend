5b4b903b8afd3f7a8cf150e1435256e56e585acb
Wojtanello-Coder <walonale@gmail.com>
2023-10-25 11:17:54 +0200

    backkend stext

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.get('/', async (req, res) => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto("http://plan.ckziu.jaworzno.pl/");
-    lessons = (await page.$$eval(".link", options => { return options.map(option => [option.innerHTML, option.href.substring(30)])}));
+    let lessons = (await page.$$eval(".link", options => { return options.map(option => [option.innerHTML, option.href.substring(30)])}));
     await browser.close();
 
     res.send(lessons);
@@ -23,25 +23,53 @@ app.get('/plan/:type/:planUrl', async (req, res) => {
     dayLength = (await page.$$eval(".hours", options => options.map(option => {
         return option.children.length - 1;
     }) ));
-    let type = req.params.type
-    lessons = await page.$$eval(".card", options => options.map(option => { // ".lesson-1,.lesson-2"
-        let table = []
-        
-            console.log(type);
-        for (let j = 0; j < option.children.length; j++) {
-            let childs;
-            if(req.params.type == "teacher"){
-                childs = option.children[j];
-            }
-            else {
-                childs = option.children[j].children[4];
-            }
-            for (const i in [0,1,2,3]) {
-                table.push(childs.children[0].children[i].innerHTML.trim())//
-            }
-        }
-        return table;
-    }) );
+    console.log(req.params.type)
+    let lessons = await page.$$eval(".card", options => options.map(option => { // ".lesson-1,.lesson-2"
+    return table;
+    switch (req.params.type) {
+        case "classid": // TBA
+    // let table = []
+    //         //table.push(option);
+    // for (let j = 0; j < option.children.length; j++) {
+    //     for (const i in [0,1,2,3]) {
+    //         table.push(option.children[j].children[4].children[0].children[i].innerHTML.trim())//
+    //     }
+    // }
+            // let table = []
+            
+            //     console.log(type);
+            // for (let j = 0; j < option.children.length; j++) {
+            //     let childs;
+            //     if(req.params.type == "teacher"){
+            //         childs = option.children[j];
+            //     }
+            //     else {
+            //         childs = option.children[j].children[4];
+            //     }
+            //     for (const i in [0,1,2,3]) {
+            //         table.push(childs.children[0].children[i].innerHTML.trim())//
+            //     }
+            // }
+            return table;
+        }) );
+            break;
+        case "teacherid":
+            // console.log("test");
+            // console.log(page);
+            // lessons = await page.$$eval(".card", options => console.log("true"));
+            // //options.map(option => { // ".lesson-1,.lesson-2"
+            // let table = [];
+            // console.log("sukces!");
+            // for (const i in [0,1,2,3]) {
+            //     table.push(option.children[0].children[i].innerHTML.trim());
+            // }
+            return table;
+            
+        //}) );0
+    
+        default:
+            break;
+    }));
     await browser.close();
     dayLength = dayLength[0];
     res.send({ "lesson": lessons, "dayLength": dayLength });
