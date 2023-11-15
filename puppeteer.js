5fd68130ad6d42a25e65fb5a9aed8fdf691815de
Wojtanello-Coder <walonale@gmail.com>
2023-11-15 11:14:45 +0100

    2 files finish

--- puppeteer.js
+++ puppeteer.js
@@ -10,22 +10,75 @@ class request {
         await browser.close();
         return lessons;
     }
-    static async getPlanTable()  {
-        console.log("test: teachertimetable");
-        return;
+
+
+    static async getPlanTable(link, type, url)  {
+        //console.log("test: teachertimetable");
+
+        const browser = await puppeteer.launch({ headless: "new" });
+        const page = await browser.newPage();
+        console.log(link + "?" + type + "=" + url);
+        await page.goto(link + "?" + type + "=" + url);
+        let dayLength = (await page.$$eval(".hours", options => options.map(option => {
+            return option.children.length - 1;
+        }) ));
+        console.log(type)
+        //return table;
+        let lessons;
+        switch (type) {
+            case "classid":
+                console.log("test");
+                lessons = await page.$$eval(".card", options => options.map(option => { // ".lesson-1,.lesson-2"
+                    let table = []
+                            //table.push(option);
+                    for (let j = 0; j < option.children.length; j++) {
+                        for (const i in [0,1,2,3]) {
+                            table.push(option.children[j].children[4].children[0].children[i].innerHTML.trim());//
+                        }
+                    }
+                    return table;
+                }));
+                break;
+            case "teacherid":
+                
+                lessons = await page.$$eval(".card", options => options.map(option => { // ".lesson-1,.lesson-2"
+                    let table = []
+                            //table.push(option);
+                    for (let j = 0; j < option.children.length; j++) {
+                        if (option.children[0].children[3]!=null) {
+                            for (const i in [0,1,2,3]) {
+                                table.push(option.children[0].children[i].innerHTML.trim());//
+                            }
+                        }
+                    }
+                    return table;
+                }));
+                break;
+                return table;
+                
+        
+            default:
+                lessons = [];
+                break;
+        }
+        await browser.close();
+        dayLength = dayLength[0];
+        return { "lesson": lessons, "dayLength": dayLength };
     }
+
+
     static async getSubstitutions(link) {
 
         const browser = await puppeteer.launch({ headless: "new" });
         const page = await browser.newPage();
         await page.goto(link);
-        substitutions = (await page.$$eval(".entry", divs => divs.map(div => {
+        let substitutions = (await page.$$eval(".entry", divs => divs.map(div => {
             let table = [];
             let tbody = div.children[div.children.length-2].children[0];
             //let r = Replacement();
             
             for (let i = 1; i < tbody.children.length; i++) {
-                table.push(tbody.children[i].children[1].innerHTML.trim())
+                table.push(tbody.children[i].children[1].innerHTML.trim());
             }
             return table;
         }) ));
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const puppeteer = require('puppeteer');
 const express = require('express');
 const cors = require('cors');
-const app = express();
 const { Replacement } = require('./replacement');
 const Reqs = require('./puppeteer');
+
+const app = express();
 app.use(cors());
 
 app.get('/', async (req, res) => {
@@ -15,88 +16,16 @@ app.get('/', async (req, res) => {
 })
 
 app.get('/plan/:type/:planUrl', async (req, res) => {
+
+    let data = await Reqs.getPlanTable("http://plan.ckziu.jaworzno.pl/", req.params.type, req.params.planUrl)
     
-    const browser = await puppeteer.launch({ headless: "new" });
-    const page = await browser.newPage();
-    console.log("http://plan.ckziu.jaworzno.pl/?" + req.params.type + "=" + req.params.planUrl);
-    await page.goto("http://plan.ckziu.jaworzno.pl/?" + req.params.type + "=" + req.params.planUrl);
-    dayLength = (await page.$$eval(".hours", options => options.map(option => {
-        return option.children.length - 1;
-    }) ));
-    console.log(req.params.type)
-    //return table;
-    let lessons;
-    switch (req.params.type) {
-        case "classid": // TBA
-            console.log("test");
-            lessons = await page.$$eval(".card", options => options.map(option => { // ".lesson-1,.lesson-2"
-                let table = []
-                        //table.push(option);
-                for (let j = 0; j < option.children.length; j++) {
-                    for (const i in [0,1,2,3]) {
-                        table.push(option.children[j].children[4].children[0].children[i].innerHTML.trim());//
-                    }
-                }
-                return table;
-            }));
-            break;
-    // }
-            // let table = []
-            
-            //     console.log(type);
-            // for (let j = 0; j < option.children.length; j++) {
-            //     let childs;
-            //     if(req.params.type == "teacher"){
-            //         childs = option.children[j];
-            //     }
-            //     else {
-            //         childs = option.children[j].children[4];
-            //     }
-            //     for (const i in [0,1,2,3]) {
-            //         table.push(childs.children[0].children[i].innerHTML.trim())//
-            //     }
-            // }
-        case "teacherid":
-            
-            lessons = await page.$$eval(".card", options => options.map(option => { // ".lesson-1,.lesson-2"
-                let table = []
-                        //table.push(option);
-                for (let j = 0; j < option.children.length; j++) {
-                    if (option.children[0].children[3]!=null) {
-                        for (const i in [0,1,2,3]) {
-                            table.push(option.children[0].children[i].innerHTML.trim());//
-                        }
-                    }
-                }
-                return table;
-            }));
-            break;
-            // console.log("test");
-            // console.log(page);
-            // lessons = await page.$$eval(".card", options => console.log("true"));
-            // //options.map(option => { // ".lesson-1,.lesson-2"
-            // let table = [];
-            // console.log("sukces!");
-            // for (const i in [0,1,2,3]) {
-            //     table.push(option.children[0].children[i].innerHTML.trim());
-            // }
-            return table;
-            
-        //}) );0
-    
-        default:
-            lessons = [];
-            break;
-    }
-    await browser.close();
-    dayLength = dayLength[0];
-    res.send({ "lesson": lessons, "dayLength": dayLength });
-    console.log({ "lesson": lessons, "dayLength": dayLength });
+    res.send(data);
+    console.log(data);
 });
 
 
 app.get('/zastepstwa', async (req, res) => {
-    let data = Reqs.getSubstitutions("https://www.ckziu.jaworzno.pl/zastepstwa/");
+    let data = await Reqs.getSubstitutions("https://www.ckziu.jaworzno.pl/zastepstwa/");
     res.send(data);
     console.log(data);
 });
