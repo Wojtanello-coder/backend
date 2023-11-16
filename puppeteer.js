58ae677d2b43de6b7d8f7dd770ddd922eb2d418f
Wojtanello-Coder <walonale@gmail.com>
2023-11-16 09:23:50 +0100

    dictionarification

--- puppeteer.js
+++ puppeteer.js
@@ -72,20 +72,69 @@ class request {
         const browser = await puppeteer.launch({ headless: "new" });
         const page = await browser.newPage();
         await page.goto(link);
-        let substitutions = (await page.$$eval(".entry", divs => divs.map(div => {
+        let substitutionObject = (await page.$$eval(".entry", divs => divs.map(div => {
             let table = [];
+            let subTable = [];
             let tbody = div.children[div.children.length-2].children[0];
             //let r = Replacement();
             
             for (let i = 1; i < tbody.children.length; i++) {
-                table.push(tbody.children[i].children[1].innerHTML.trim());
+                let subObj = {};
+                //     "class": "",
+                //     "teacher": "",
+                //     "hour": 0,
+                //     "subject": "",
+                //     "cancelled": false,
+                //     "substitution": "",
+                //     "subHour": 0,
+                //     "subSubject": "",
+                //     "subRoom": ""
+
+                // }
+                let tbody_inner = tbody.children[i];
+                if (tbody_inner.children.length == 5) {
+                    console.log("5");
+                    table.push([]);
+                    table.at(-1).push(tbody_inner.children[1].innerHTML.trim());
+                }
+                else {
+                    table.at(-1).push(tbody_inner.children[0].innerHTML.trim());
+                }
+                let len = tbody_inner.children.length;
+                let hours = tbody_inner.children[len - 4].innerHTML.trim().split("->");
+                let subjects = tbody_inner.children[len - 2].innerHTML.trim().split("->");
+                subObj["teacher"] = tbody_inner.children[len - 3].innerHTML.trim();
+                subObj["hour"] = hours[0];
+                subObj["subject"] = subjects[0];
+
+                (hours.length>1)?
+                    subObj["subHour"] = hours[1]
+                :
+                    subObj["subHour"] = "a"
+
+                (subjects.length>1)?
+                    subObj["subSubjects"] = subjects[1]
+                :
+                    subObj["subSubjects"] = "b"
+                
+                
+                subTable.push(subObj);
             }
-            return table;
+            let subListObj = {
+                "desc": "opis",
+                "date": "data",
+                "teachers": [
+                    // nieobecni nauczyciele
+                ],
+                "substitutions": subTable
+            }
+            //return table;
+            return subListObj;
         }) ));
         //html = (await page.$$eval("body", divs => divs.map(div => div)));
         //console.log(substitutions);
         await browser.close();
-        return substitutions;
+        return substitutionObject;
     }
 }
 
