5dac27c6affe5b50e20601f86d9dc4268e51125c
Oskar Zieliński <>
2023-11-17 15:47:07 +0100

    Atlas

--- server.js
+++ server.js
@@ -7,6 +7,30 @@ const Reqs = require('./puppeteer');
 const app = express();
 app.use(cors());
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://multiplan:cOZDVaSF7SZZmg9l@cluster0.pdmzibl.mongodb.net/?retryWrites=true&w=majority";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 app.get('/', async (req, res) => {
     console.log(`${Date()} - ${req.method} request at ${req.path}`)
     let data = [];
@@ -40,7 +64,6 @@ app.get('/plan/:type/:planUrl', async (req, res) => {
     res.send(data);
     //console.log(data);
 });
-
 
 app.get('/zastepstwa', async (req, res) => {
     console.log(`${Date()} - ${req.method} request at ${req.path}`)
